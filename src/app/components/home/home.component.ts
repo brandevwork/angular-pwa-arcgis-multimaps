6e611e727e3140611b1e927704e2926284da46be
silversoftdev <kennethberthelotwork@gmail.com>
2022-02-04 11:32:53 +0100

    feat: updated to latest webView method, new map

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,27 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import EsriMap from '@arcgis/core/Map';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+import WebMap from '@arcgis/core/WebMap';
+import Sketch from '@arcgis/core/widgets/Sketch';
 import MapView from '@arcgis/core/views/MapView';
+import esriConfig from '@arcgis/core/config';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   title = 'angular-arcgis-multimaps';
 
-  constructor() { }
+  public view: any = null;
+  // The <div> where we will place the map
+  @ViewChild('mapView', { static: true }) private mapViewEl!: ElementRef;
 
-  ngOnInit(): void {
-    const webmap = new EsriMap({
-      basemap: 'streets',
+  initializeMap(): Promise<any> {
+    const container = this.mapViewEl.nativeElement;
+
+    const webmap = new WebMap({
+      portalItem: {
+        // id: 'aa1d3f80270146208328cf66d022e09c',
+        id: 'e691172598f04ea8881cd2a4adaa45ba'
+      },
     });
+
     const view = new MapView({
-      container: 'viewDiv',
-      center: [-4.6417656, 57.2741432], //Longitude, Latitude
-      zoom: 7,
+      container,
       map: webmap,
     });
+
+    this.view = view;
+    const gLayer = new GraphicsLayer();
+    webmap.add(gLayer);
+
+    const sketch = new Sketch({
+      view,
+      layer: gLayer
+    });
+    view.ui.add(sketch, "top-right");
+    return this.view.when();
   }
 
+  ngOnInit(): any {
+    esriConfig.assetsPath = '/assets/';
+    this.initializeMap()
+  }
+
+  ngOnDestroy(): void {
+    if (this.view) {
+      this.view.destroy();
+    }
+  }
 }
